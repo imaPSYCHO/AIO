1889a65551a0ba747a15cc934e920fae92b0c776
imaPSYCHO <74499960+imaPSYCHO@users.noreply.github.com>
2021-06-08 02:59:06 +0800

    Add files via upload

---
+++ part2.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+# Debian and Ubuntu VPS Installer
+# Script by Juan
+# 
+# Illegal selling and redistribution of this script is strictly prohibited
+# Please respect author's Property
+# Binigay sainyo ng libre, ipamahagi nyo rin ng libre.
+#
+#
+
+
+ # Now check if our machine is in root user, if not, this script exits
+ # If you're on sudo user, run `sudo su -` first before running this script
+ 
+ if [[ $EUID -ne 0 ]];then
+ ScriptMessage
+ echo -e "[\e[1;31mError\e[0m] This script must be run as root, exiting..."
+ exit 1
+fi
+
+
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+SYS_DT=$(date +%F-%T | tr ':' '_')
+
+exiterr()  { echo "Error: $1" >&2; exit 1; }
+conf_bk() { /bin/cp -f "$1" "$1.old-$SYS_DT" 2>/dev/null; }
+
+add_vpn_user() {
+
+if [ "$(id -u)" != 0 ]; then
+  exiterr "Script must be run as root. Try 'sudo sh $0'"
+fi
+
+if [ ! -f "/etc/ppp/chap-secrets" ] || [ ! -f "/etc/ipsec.d/passwd" ]; then
+cat 1>&2 <<'EOF'
+Error: File /etc/ppp/chap-secrets and/or /etc/ipsec.d/passwd do not exist!
+       Your must first set up the VPN server before adding VPN users.
+       See: https://github.com/hwdsl2/setup-ipsec-vpn
+EOF
+  exit 1
+fi
+
+if ! grep -qs "hwdsl2 VPN script" /etc/sysctl.conf; then
+cat 1>&2 <<'EOF'
+Error: This script can only be used with VPN servers created using:
+       https://github.com/hwdsl2/setup-ipsec-vpn
+EOF
+  exit 1
+fi
+
+VPN_USER=$1
+VPN_PASSWORD=$2
+
+if [ -z "$VPN_USER" ] || [ -z "$VPN_PASSWORD" ]; then
+cat 1>&2 <<EOF
+Usage: sudo sh $0 'username_to_add' 'password_to_add'
+EOF
+  exit 1
+fi
+
+if printf '%s' "$VPN_USER $VPN_PASSWORD" | LC_ALL=C grep -q '[^ -~]\+'; then
+  exiterr "VPN credentials must not contain non-ASCII characters."
+fi
+
+case "$VPN_USER $VPN_PASSWORD" in
+  *[\\\"\']*)
+    exiterr "VPN credentials must not contain these special characters: \\ \" '"
+    ;;
+esac
+
+clear
+
+cat <<EOF
+
+Welcome! This script will add or update an VPN user account
+for both IPsec/L2TP and IPsec/XAuth (Cisco IPsec).
+
+If the username you specified matches an existing VPN user,
+that user will be updated with the new password.
+
+Please double check before continuing!
+
+================================================
+
+VPN user to add or update:
+
+Username: $VPN_USER
+Password: $VPN_PASSWORD
+
+Write these down. You'll need them to connect!
+
+================================================
+
+EOF
+
+# Backup config files
+conf_bk "/etc/ppp/chap-secrets"
+conf_bk "/etc/ipsec.d/passwd"
+
+# Add or update VPN user
+sed -i "/^\"$VPN_USER\" /d" /etc/ppp/chap-secrets
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" l2tpd "$VPN_PASSWORD" *
+EOF
+
+# shellcheck disable=SC2016
+sed -i '/^'"$VPN_USER"':\$1\$/d' /etc/ipsec.d/passwd
+VPN_PASSWORD_ENC=$(openssl passwd -1 "$VPN_PASSWORD")
+cat >> /etc/ipsec.d/passwd <<EOF
+$VPN_USER:$VPN_PASSWORD_ENC:xauth-psk
+EOF
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+
+cat <<'EOF'
+Done!
+
+NOTE: All VPN users will share the same IPsec PSK.
+  If you forgot the PSK, check /etc/ipsec.secrets.
+
+EOF
+
+}
+
+## Defer until we have the complete script
+add_vpn_user "$@"
+
+exit 0
